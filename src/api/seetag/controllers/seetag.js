11dd411f91a9e43069ec8912d2eb3c1983564b68
scurrypf <2549018667@qq.com>
2023-02-27 15:31:46 +0800

    article接口补充&添加二级tag接口

---
+++ src/api/seetag/controllers/seetag.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * seetag controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::seetag.seetag');
---
+++ src/api/seetag/services/seetag.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * seetag service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::seetag.seetag');
